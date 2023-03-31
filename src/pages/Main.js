a4528656edc0f1a6e733f2790b3a7886abba5c20
Isakova Mariya <122015067+IsakovaMary@users.noreply.github.com>
2023-03-31 20:20:01 +0300

    Edit 1.0

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,56 +5,85 @@ import imgSearch from './search.svg';
 
 const Main = () =>  {
 
-   
-    
     const [photo, setPhoto] = useState("");
     const [result, setResult] = useState([]);
     const [isLoading, setisLoading] = useState(false);
 
     const changePhoto = () => {
-        axios.get(`https://api.unsplash.com/search/photos?page=1&query=${photo}&client_id=Ip0XA55zY7b7-d19osq1L5btGg-YCeDZVpnnJjXqHxs`)
+        axios.get(`https://api.unsplash.com/search/photos?page=1&query=${photo}&client_id=4UhQL7IdZ55_AGB-WhQQNVlxTwun4XNQI0lXn6L-gqM`)
             .then((response) => {
                 // console.log(response.data);
                 setResult(response.data.results);
                 setisLoading(false);
             })
+
+            // Добавление затемняющего фона
+            const foo = document.querySelector('.wrapper-overlay');
+            foo.classList.remove('form-focus');
+            foo.classList.add('form-overlay')
+
+            // Поднятие форма вверх
+            const top = document.querySelector('.back');
+            top.classList.add('search-area-top')
+
+            const hideInput = document.querySelector('.form-search-begin');
+            hideInput.classList.add('form-search-begin-hide')
+            const visibleInput = document.querySelector('.form-search');
+            visibleInput.classList.add('form-search-visible')
+
     }
 
-    const onKeyDown = e => {
-        if(e.key === 'Enter') {
-            axios.get(`https://api.unsplash.com/search/photos?page=1&query=${photo}&client_id=Ip0XA55zY7b7-d19osq1L5btGg-YCeDZVpnnJjXqHxs`)
-            .then((response) => {
-                console.log(response.data);
-                setResult(response.data.results);
-                setisLoading(false);
-            })
-        }}
-        
+    const handleClick = e => {
+        const foo = document.querySelector('.wrapper-overlay');
+        foo.classList.toggle('form-focus');
+      };
+
+    // const onKeyDown = e => {
+    //     if(e.key === 'Enter') {
+    //         axios.get(`https://api.unsplash.com/search/photos?page=1&query=${photo}&client_id=4UhQL7IdZ55_AGB-WhQQNVlxTwun4XNQI0lXn6L-gqM`)
+    //         .then((response) => {
+    //             console.log(response.data);
+    //             setResult(response.data.results);
+    //             setisLoading(false);
+    //         })
+    //     }}
+
+       
 
     return (
         <>
         <div className='wrapper'>
-            <div className='container search-area'>
-                <input type="search"  className='form-control form-search' onKeyDown={onKeyDown} value={photo} onChange={(e) => {
+        <div className='back'>
+            <div className='search-area'>
+                
+                <input type="search"  className='form-control form-search-begin' value={photo} onChange={(e) => {
+                    setPhoto(e.target.value)
+                }} />
+                <input type="search"  className='form-control form-search' onClick={handleClick} value={photo} onChange={(e) => {
                     setPhoto(e.target.value)
                 }} />
                 <img src={imgSearch} className="imgSearch" alt="search"/>
                 <button type='search' onClick={changePhoto} className='btn btn-search '>Искать</button>
+                </div>
             </div>
 
                 {isLoading? <Loader/> : 
-                <div className="container">
-                    <div className="row">
-                        {result.map((value, index) => {
-                            return (
-                                <div className="img-result col-lg-2 col-md-3 col-xs-4" key={index}>
-                                        <img className="img-fluid wrapper" src={value.urls.small} alt='' />
-                                </div>
-                            )
-                        })}
+                
+                    <div className="container">
+                        <div className="row">
+                            {result.map((value, index) => {
+                                return (
+                                    <div className="img-result col-lg-2 col-md-3 col-xs-4" key={index}>
+                                            <img className="img-fluid wrapper" src={value.urls.small} alt='' />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className='wrapper-overlay'>
                     </div>
-
-                </div> }
+                    </div> 
+                
+                }
 
         </div> 
         </>
